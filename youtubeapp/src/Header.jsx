ba397050612a771329cee30bbd693ba6980df295
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-02-07 09:31:23 +0530

    second Commit

---
+++ youtubeapp/src/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>I am youtube header</h1>
+    </div>
+  )
+}
+
+export default Header
+
+
+// Extension- es7 snippets
+/*
+
+MenuIcon-filled
+className(header_logo)youtube logo- https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png
+
+input filed
+SearchIcon
+
+VideoCallICon
+AppsIcon
+NotificationsIcon
+Avatar->alt->src
+
+header.css
+three div ->flexbox -> space_between ->
+
+
+header->header_left->header_input->header__icon
+height= 25px
+object-fit= contain
+padding=20px
+*/
